721626b07636806ab2671adcd0da298e478410b2
YuriyLavrinchuk <yuriy.lavrinchuk@gmail.com>
2017-04-03 22:46:25 +0300

    Добавлен INSERT и переработан SELECT

--- src/main/java/connection/DBconnection.java
+++ src/main/java/connection/DBconnection.java
@@ -34,107 +34,24 @@ public class DBconnection {
 
     public DBconnection dbConnection() throws SQLException, ClassNotFoundException, InvalidException {
 
-        Connection connection = null;
         if(!checkParametrs()) throw new InvalidException("Invalid incoming parameter:" + " dbname: "+ dbname + "username: " + username + " password: " + password);
-       //1-й вариант
-        /*  else{
+          else{
             Class.forName("org.postgresql.Driver");
             Connection connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Connection success to: " + dbname);
-        }*/
+//            System.out.println("Connection success to: " + dbname);
 
-      //2-й вариант
-        else
-        try {
-            //Загружаем драйвер
-            Class.forName("org.postgresql.Driver");
-//            System.out.println("Драйвер подключен");
-            //Создаём соединение
-            connection = DriverManager.getConnection(url, username, password);
-            System.out.println("Connection success to: " + dbname);
-            //Для использования SQL запросов существуют 3 типа объектов:
-            //1.Statement: используется для простых случаев без параметров
-            Statement statement = null;
+            Statement statement = connection.createStatement();
+            //INSERT
+//            statement.executeUpdate("INSERT INTO users VALUES('4', 'user3', '1258')");
 
-            statement = connection.createStatement();
-            //Выполним запрос
-            ResultSet select1 = statement.executeQuery(
-                    "SELECT * FROM users");
-            //result это указатель на первую строку с выборки
-            //чтобы вывести данные мы будем использовать
-            //метод next() , с помощью которого переходим к следующему элементу
-            System.out.println("Output select");
-            while (select1.next()) {
-                System.out.println("Номер в выборке #" + select1.getRow()
-                        + "\t id #" + select1.getInt("id")
-                        + "\t" + select1.getString("name")
-                        + "\t" + select1.getString("password"));
-            }
-            // Вставить запись
-//            statement.executeUpdate(
-//                    "INSERT INTO users(username) values('name')");
-            //Обновить запись
-//            statement.executeUpdate(
-//                    "UPDATE users SET username = 'admin' where id = 1");
-
-
-
-            //2.PreparedStatement: предварительно компилирует запросы,
-            //которые могут содержать входные параметры
-//            PreparedStatement preparedStatement = null;
-            // ? - место вставки нашего значеня
-//            preparedStatement = connection.prepareStatement(
-//                    "SELECT * FROM users where id > ? and id < ?");
-            //Устанавливаем в нужную позицию значения определённого типа
-//            preparedStatement.setInt(1, 2);
-//            preparedStatement.setInt(2, 10);
-            //выполняем запрос
-//            ResultSet result2 = preparedStatement.executeQuery();
-//
-//            System.out.println("Выводим PreparedStatement");
-//            while (result2.next()) {
-//                System.out.println("Номер в выборке #" + result2.getRow()
-//                        + "\t Номер в базе #" + result2.getInt("id")
-//                        + "\t" + result2.getString("username"));
-//            }
-//
-//            preparedStatement = connection.prepareStatement(
-//                    "INSERT INTO users(username) values(?)");
-//            preparedStatement.setString(1, "user_name");
-            //метод принимает значение без параметров
-            //темже способом можно сделать и UPDATE
-//            preparedStatement.executeUpdate();
-
-
-
-            //3.CallableStatement: используется для вызова хранимых функций,
-            // которые могут содержать входные и выходные параметры
-//            CallableStatement callableStatement = null;
-            //Вызываем функцию myFunc (хранится в БД)
-//            callableStatement = connection.prepareCall(
-//                    " { call myfunc(?,?) } ");
-            //Задаём входные параметры
-//            callableStatement.setString(1, "Dima");
-//            callableStatement.setString(2, "Alex");
-            //Выполняем запрос
-//            ResultSet result3 = callableStatement.executeQuery();
-            //Если CallableStatement возвращает несколько объектов ResultSet,
-            //то нужно выводить данные в цикле с помощью метода next
-            //у меня функция возвращает один объект
-//            result3.next();
-//            System.out.println(result3.getString("MESSAGE"));
-            //если функция вставляет или обновляет, то используется метод executeUpdate()
-
-        } catch (Exception ex) {
-            //выводим наиболее значимые сообщения
-            Logger.getLogger(DBconnection.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ex) {
-                    Logger.getLogger(DBconnection.class.getName()).log(Level.SEVERE, null, ex);
-                }
+            //SELECT
+            ResultSet select = statement.executeQuery("select * from users");
+            while (select.next()){
+                System.out.println("Номер в выборке #" + select.getRow()
+                        + "\t" + select.getInt("id")
+                        + "\t" + select.getString("name")
+                        + "\t" + select.getString("password")
+                );
             }
         }
         return null;
