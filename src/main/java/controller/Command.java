45db0c7fe1144e56ae743fad742a35c9bfe22a6f
YuriyLavrinchuk <yuriy.lavrinchuk@gmail.com>
2017-04-03 22:08:23 +0300

    Реализован новый метод подключения к ДБ. Также добавлен вывод запроса из таблицы.

---
+++ src/main/java/controller/Command.java
@@ -0,0 +1,8 @@
+package controller;
+
+/**
+ * Created by Admin on 03.04.2017.
+ */
+public interface Command {
+    public void execute();
+}
---
+++ src/main/java/controller/ConnectToDB.java
@@ -0,0 +1,9 @@
+package controller;
+
+/**
+ * Created by Admin on 03.04.2017.
+ */
+public class ConnectToDB implements Command {
+    public void execute() {
+    }
+}
