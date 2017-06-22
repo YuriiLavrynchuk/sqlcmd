6edb43dbe9986e88bd6c56e00aa9a655dcb796aa
yuriylavrinchuk <yuriy.lavrinchuk@gmail.com>
2017-06-22 17:03:06 +0300

    Продолжил реализовывать Integration Test

--- src/test/java/integration/ConfigurableInputStream.java
+++ src/test/java/integration/ConfigurableInputStream.java
@@ -1,16 +1,46 @@
 package integration;
 
+import view.DataInOut;
+
 import java.io.IOException;
 import java.io.InputStream;
 
-public class ConfigurableInputStream extends InputStream{
+public class ConfigurableInputStream extends InputStream implements DataInOut {
     private String line;
 
     @Override
     public int read() throws IOException {
-//        char ch = line.indexOf(0);
+        if (line.length() == 0) {
+            return -1;
+        }
+        char ch = line.charAt(0);
         line = line.substring(1);
 
         return 1;
+    }
+
+    public void add(String line){
+        if(this.line == null) {
+            this.line = line;
+        } else {
+            this.line += "\n" + line;
+        }
+    }
+
+    public String getLine(){
+        add(line);
+        return line;
+    }
+
+    @Override
+    public void outPut(String message) {
+
+    }
+
+    @Override
+    public String inPut() {
+//        Scanner scanner = new Scanner(System.in);
+
+        return line;
     }
 }
--- src/test/java/integration/IntegrationTest.java
+++ src/test/java/integration/IntegrationTest.java
@@ -1,22 +1,37 @@
 package integration;
 
+import controller.MainController;
+import exeption.InvalidException;
+import model.DBconnection;
 import org.junit.Before;
 import org.junit.Test;
+import view.DataInOut;
 
 import java.io.PrintStream;
+import java.sql.Connection;
+
+import static org.junit.Assert.assertEquals;
 
 public class IntegrationTest {
 
     private static ConfigurableInputStream in;
-    private static LogPrintStream out;
+    private static LogOutputStream out;
 
     @Before
-    public static void setup() {
+    public /*static*/ void setup() throws InvalidException {
+        in = new ConfigurableInputStream();
+        out = new LogOutputStream();
+
         System.setIn(in);
         System.setOut(new PrintStream(out));
     }
     @Test
-    public void testExit() {
+    public void testExit() throws InvalidException {
+        Connection connectToDB = new DBconnection("postgres", "postgres", "1234").dbConnection();
+        in.add("help");
+        in.add("exit");
 
+        new MainController((DataInOut)in, connectToDB).start();
+        assertEquals("", out.getData());
     }
 }
