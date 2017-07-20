9b8f9734abfc05093140670b2744427246269b93
yuriylavrinchuk <yuriy.lavrinchuk@gmail.com>
2017-07-20 10:56:38 +0300

    Закончил тесты ExSelect, ExDelete и добавил изменения в FakeDataInOut

---
+++ src/test/java/controller/command/ExDeleteTestWithMock.java
@@ -0,0 +1,39 @@
+package controller.command;
+
+import controller.Command;
+import controller.ExDelete;
+import model.DbConnection;
+import model.Delete;
+import org.junit.Before;
+import org.junit.Test;
+import view.DataInOut;
+
+import java.sql.SQLException;
+
+import static org.mockito.Mockito.*;
+
+public class ExDeleteTestWithMock {
+    private DbConnection dbConnection;
+    private DataInOut dataInOut;
+    private Delete delete;
+    private Command command;
+
+    @Before
+    public void setup() throws SQLException {
+        dataInOut = mock(DataInOut.class);
+        dbConnection = mock(DbConnection.class);
+        delete = mock(Delete.class);
+        command = new ExDelete(dataInOut, dbConnection, delete);
+    }
+
+    @Test
+    public void testExDelete() throws Exception {
+        //when
+        when(dataInOut.inPut()).thenReturn("delete from 'users'");
+
+        command.execute("delete");
+
+        //then
+        verify(dataInOut).outPut("Enter Delete query:");
+    }
+}
